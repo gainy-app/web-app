9cfdfc1ccb0ab71e64d9eb01db53102257ecdce3
Valentine Kalchevski <valtikamore@gmail.com>
2022-11-24 17:54:59 +0300

    defect/change-login-screens-layout: Update styles on login screens

--- src/components/common/Button/index.tsx
+++ src/components/common/Button/index.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 interface Props extends React.HTMLProps<HTMLButtonElement> {
     children: JSX.Element | string | JSX.Element[]
     onClick?: () => void
-    variant?: 'apple' | 'transparent'
+    variant?: 'apple' | 'google'
     type?: 'button' | 'submit' | 'reset'
 }
 
@@ -14,7 +14,7 @@ export const Button = ({children, onClick, variant, ...rest}: Props) => {
     <button
       className={classNames(styles.button, {
         [styles.button_apple]: variant === 'apple',
-        [styles.button_transparent]: variant === 'transparent',
+        [styles.button_google]: variant === 'google',
       })}
       onClick={onClick}
       {...rest}
--- src/routes/SignIn/config.ts
+++ src/routes/SignIn/config.ts
@@ -1,6 +1,7 @@
 export const config = {
-  title: 'Sign in with one of the following options to start investing.',
-  description: 'Welcome to Gainy, a new platform for smart investors. Discover thematic stock collections and other investment ideas to make your portfolio better and more diversified for long-term results.',
+  title: 'Welcome to Gainy, a new platform for smart investors.',
+  description: 'Discover thematic stock collections and other investment ideas to make your portfolio better and more diversified for long-term results.',
+  subDescription: 'Sign in with one of the following options to start investing.',
   form: {
     apple: 'Enter with Apple',
     google: 'Enter with Google'
