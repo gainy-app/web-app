63386a1fd81a7e1e4b75bae74ec324deb1317c45
Valentine Kalchevski <valtikamore@gmail.com>
2022-11-24 13:51:47 +0300

    defect/change-bg-color: Change bg color

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,22 +1,24 @@
 import { Footer } from './footer';
 import { Header } from './header';
 import styles from './layout.module.scss';
-import {usePage} from 'hooks';
+import { useLocation } from 'react-router-dom';
+import { routes } from 'utils/constants';
 
 interface Props {
     children?: JSX.Element
 }
 
 export const Layout = ({children}: Props) => {
-  const {withHeader} = usePage();
+  const {pathname} = useLocation();
+  const withHeader = (!pathname.includes(routes.signIn) && !pathname.includes(routes.getApp));
 
   return (
-    <>
+    <div className={`${styles.container} ${withHeader ? '' : styles.blue}`}>
       {withHeader && <Header />}
       <main className={styles.content}>
         {children}
       </main>
       <Footer footerClassName={styles.footer}/>
-    </>
+    </div>
   );
 };
