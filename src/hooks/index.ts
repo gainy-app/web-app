8584028f68cd80d386f1c85bd534eab4642e9734
Valentine Kalchevski <valtikamore@gmail.com>
2022-11-24 14:29:51 +0300

    defect/change-bg-color: Update general layout

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,1 +1,11 @@
-export const a = {};
+import {useLocation} from 'react-router-dom';
+import {routes} from 'utils/constants';
+
+export const usePage = () => {
+  const {pathname} = useLocation();
+  const withHeader = (!pathname.includes(routes.signIn) && !pathname.includes(routes.getApp));
+
+  return {
+    withHeader
+  };
+};
