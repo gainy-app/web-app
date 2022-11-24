13cb818601aee389d67bb07000736f6218524686
Valentine Kalchevski <valtikamore@gmail.com>
2022-11-24 14:29:51 +0300

    defect/change-bg-color: Update general layout

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,24 +1,22 @@
 import { Footer } from './footer';
 import { Header } from './header';
 import styles from './layout.module.scss';
-import { useLocation } from 'react-router-dom';
-import { routes } from 'utils/constants';
+import {usePage} from 'hooks';
 
 interface Props {
     children?: JSX.Element
 }
 
 export const Layout = ({children}: Props) => {
-  const {pathname} = useLocation();
-  const withHeader = (!pathname.includes(routes.signIn) && !pathname.includes(routes.getApp));
+  const {withHeader} = usePage();
 
   return (
-    <div className={`${styles.container} ${withHeader ? '' : styles.blue}`}>
+    <>
       {withHeader && <Header />}
       <main className={styles.content}>
         {children}
       </main>
       <Footer footerClassName={styles.footer}/>
-    </div>
+    </>
   );
 };
--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,8 +1,8 @@
-import {useLocation} from 'react-router-dom';
-import {routes} from 'utils/constants';
+import { useLocation } from 'react-router-dom';
+import { routes } from 'utils/constants';
 
 export const usePage = () => {
-  const {pathname} = useLocation();
+  const { pathname } = useLocation();
   const withHeader = (!pathname.includes(routes.signIn) && !pathname.includes(routes.getApp));
 
   return {
