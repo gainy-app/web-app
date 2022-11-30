054f3567a7baa449ab50f305737e65d8fb629a9f
Valentine Kalchevski <valtikamore@gmail.com>
2022-11-30 12:11:16 +0300

    feature/remove-resize fix container

--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -1,20 +1,25 @@
-import { Layout, Invest } from 'components';
+import { Layout
+  // , Invest
+} from 'components';
 import styles from './home.module.scss';
-import { config } from './config';
-import { NumberFormatValues } from 'react-number-format';
-import React, { useState } from 'react';
+// import { config } from './config';
+// import { NumberFormatValues } from 'react-number-format';
+import React
+  // , { useState }
+  from 'react';
 
 export default function Home () {
-  const { invest } = config;
-  const [sum, setSum] = useState<string | null>(null);
-
-  const onSumChange = (values: NumberFormatValues) => {
-    setSum(values.formattedValue);
-  };
+  // const { invest } = config;
+  // const [sum, setSum] = useState<string | null>(null);
+  //
+  // const onSumChange = (values: NumberFormatValues) => {
+  //   setSum(values.formattedValue);
+  // };
 
   return (
     <Layout footerClassName={styles.footer}>
-      <Invest sum={sum} onSumChange={onSumChange} invest={invest}/>
+      {/*<Invest sum={sum} onSumChange={onSumChange} invest={invest}/>*/}
+
     </Layout>
   );
 }
