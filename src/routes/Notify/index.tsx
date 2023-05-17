3c4468e8348c6c34dd52a62ba1c2db402593c751
Dmitry_savenkov <dmitry2002savenkov@mail.ru>
2023-05-17 12:40:56 +0300

    feat: updated notify page design

--- src/routes/Notify/index.tsx
+++ src/routes/Notify/index.tsx
@@ -8,8 +8,8 @@ import { formatNumber, getCurrentYear, getQueryAppLink, parseGQLerror } from '..
 import { useMutation } from '@apollo/client';
 import { SEND_APP_LINK } from '../../services/gql/queries';
 import { config } from './config';
-import { Background } from '../Success/Background';
 import { sendEvent } from 'utils/logEvent';
+import CosmonautImage from '../../assets/cosmonaut.png';
 import { useFormContext } from 'contexts/FormContext';
 import { useAuth } from 'contexts/AuthContext';
 import { useLocation } from 'react-router-dom';
@@ -57,61 +57,72 @@ export default function Notify () {
   };
 
   return (
-    <>
-      <main className={styles.main}>
-        <section className={styles.section}>
-          <Image type={imageTypes.logoWhite} className={styles.logo}/>
-          <h2 className={styles.mainTitle}>You’ll be notified when</h2>
-          <h2 className={styles.title}>{`Trading feature will be available in ${notifyCountry}`}</h2>
-          <p className={styles.subtitle}>{subtitle}</p>
-          <ButtonLink
-            href={downloadButton.link}
-            onClick={handleDownloadButtonClick}
-            variant={'download'}
-            id={downloadButton.id}
-          >
-            {downloadButton.text}
-          </ButtonLink>
+    <section className={styles.section}>
+      <div className={styles.contentWrapper}>
+        <div className={styles.infoWrapper}>
+          <div className={styles.logoWrapper}>
+            <Image type={imageTypes.logoWhite} className={styles.logo}/>
+          </div>
+          <h1 className={styles.title}>You’ll be notified when</h1>
+          <p className={styles.subtitle}>Trading feature will be available</p>
+          <p className={styles.countryAvailable}>in {notifyCountry}</p>
+          <p className={styles.note}>{subtitle}</p>
+          <div className={styles.buttonWrapper}>
+            <ButtonLink
+              href={downloadButton.link}
+              onClick={handleDownloadButtonClick}
+              variant={'download'}
+              id={downloadButton.id}
+            >
+              {downloadButton.text}
+            </ButtonLink>
+          </div>
+          <div className={styles.cosmonautMobileWrapper}>
+            <img src={CosmonautImage} loading='lazy' alt='cosmonaut' width="100%" height="100%"/>
+          </div>
           <div className={styles.line}>
             <Image type={imageTypes.line}/>
           </div>
           <div className={styles.qrWrapper} id={qrcode.id}>
             <QRCodeSVG value={qrcode.link} className={styles.qrCode}/>
           </div>
-          <p className={styles.description}>{description}</p>
-          <form
-            onSubmit={onSubmitHandler}
-          >
-            <div className={styles.actions}>
-              <Input>
-                <PatternFormat
-                  placeholder={form.phone}
-                  valueIsNumericString
-                  format="(###) ###-####"
-                  mask="_"
-                  name={'phone'}
-                  onValueChange={onPhoneChange}
-                  value={phoneState}
-                  className={`${errors ? styles.errorValidation : ''}`}
-                />
-              </Input>
-              <Button type={'submit'} id={'webapp_signin_send_link'}>
-                {loading ? <Loader className={styles.loader}/> : form.button}
-              </Button>
-              {error && (
-                <p className={styles.error}>{parseGQLerror(error)}</p>
-              )}
-              {data?.send_app_link?.ok && (
-                <p className={styles.success}>{form.successMessage}</p>
-              )}
-            </div>
-          </form>
-        </section>
-        <Background />
-      </main>
-      <footer className={styles.footer}>
-        <span>© {currentYear} Gainy, Inc. </span>
-      </footer>
-    </>
+          <p className={styles.qrDescription}>{description}</p>
+          <div className={styles.formWrapper}>
+            <form onSubmit={onSubmitHandler} >
+              <div className={styles.actions}>
+                <Input>
+                  <PatternFormat
+                    placeholder={form.phone}
+                    valueIsNumericString
+                    format="(###) ###-####"
+                    mask="_"
+                    name={'phone'}
+                    onValueChange={onPhoneChange}
+                    value={phoneState}
+                    className={`${errors ? styles.errorValidation : ''}`}
+                  />
+                </Input>
+                <Button type={'submit'} id={'webapp_signin_send_link'}>
+                  {loading ? <Loader className={styles.loader}/> : form.button}
+                </Button>
+                {error && (
+                  <p className={styles.error}>{parseGQLerror(error)}</p>
+                )}
+                {data?.send_app_link?.ok && (
+                  <p className={styles.success}>{form.successMessage}</p>
+                )}
+              </div>
+            </form>
+          </div>
+          <footer className={styles.footer}>
+            <span>© {currentYear} Gainy, Inc. </span>
+          </footer>
+        </div>
+        <div className={styles.cosmonautWrapper}>
+          <img src={CosmonautImage} loading='lazy' alt='cosmonaut' width="100%" height="100%"/>
+        </div>
+      </div>
+      <div className={styles.starsBackground} />
+    </section>
   );
 }
